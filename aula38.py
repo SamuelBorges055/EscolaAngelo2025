5776bca8fa9247e91de44979fa157306572eb313
Samuel Borges Zamengo De Souza <SamuelBorges055@hotmail.com>
2025-04-15 08:13:35 -0300

    Add files via upload

---
+++ aula38.py
@@ -0,0 +1,9 @@
+# EXERCÍCIO MATRIZ 3x3 (Tipo Jogo da Velha)
+
+# Crie uma matriz 3x3 com cada elemento prenchido com valores sequenciais de 1 a 9
+# Imprima cada linha da matriz em um linha diferente.
+# Solicite ao usário escolher uma das nove posições digitando o número correspondente
+# Verifique se o usuário digitou mesmo um número válido e dentro do intervalo de 1 a 9
+# Substitua o valor escolhido por X (letra X maiúscula) ou por O (letra O maiúscula) alternadamente começando por X
+# Se a casa escolhida pelo usuário já tiver sido preenchida antes por X ou O, imprima uma mensagem informando e solicite novamente outra opção válida.
+# Quando todas as 9 casas da matriz tiver sido preenchidas, avise o usuário e termine o programa.
